8157bc32fec29809eadf3dcbc2c3fdcd7f777a63
Mazekaz <zanym1102@gmail.com>
2021-03-24 09:17:33 +0700

    StructSetName

--- ImagetoExcel.py
+++ ImagetoExcel.py
@@ -9,7 +9,10 @@ def ReadExcelRoot(pathNameExcelRoot):
     # WBsheetData = wb['label']
     WBsheetData = wb.active
     
+    # Total Row in Excel
     maxRow = WBsheetData.max_row+1
+
+    # Ignore Fist Row because It is Header/Title
     start = 2
     while (start <= maxRow):
         # listCount.append(f"{(WBsheetData.cell(row=start, column=1).value)} | {(WBsheetData.cell(row=start, column=2).value)}")
@@ -18,7 +21,7 @@ def ReadExcelRoot(pathNameExcelRoot):
         yield TextCount
         start += 1
 
-def ResizeImages(pathNameExcelRoot, folderImages, pathNameExcelWrite):
+def ExportImagesToExcel(pathNameExcelRoot, folderImagesRoot, pathNameExcelWrite):
     # Create folder contain Excels safely
     ExportExcelFolder = "ExportExcelFolder"
     Path(ExportExcelFolder).mkdir(parents=True, exist_ok=True)
@@ -27,6 +30,7 @@ def ResizeImages(pathNameExcelRoot, folderImages, pathNameExcelWrite):
     workbook = xlsxwriter.Workbook(f"{ExportExcelFolder}/{pathNameExcelWrite}")
     worksheet = workbook.add_worksheet()
 
+    # Height Image and Cell
     cell_height = 30.0
 
     # Set Default Row
@@ -37,44 +41,49 @@ def ResizeImages(pathNameExcelRoot, folderImages, pathNameExcelWrite):
     TextCount = ReadExcelRoot(pathNameExcelRoot)
 
     numberA = numberB = numberC = 1
-    for T in TextCount:
-        TextToFile = T.split('|||')
-        # print(TextToFile[1])
-        try:
-            # Parameters Image to Row Excel
-            filename = f"{folderImages}/{TextToFile[0]}.png"
-            img = Image.open(filename)
-            image_width, image_height = img.size
-            y_scale = cell_height/image_height
-            cellImage = 'B' + str(numberB)
-            # Insert Image to Row Excel
-            worksheet.insert_image(cellImage, filename, {'x_scale': y_scale, 'y_scale': y_scale})
+    try:
+        for T in TextCount:
+            TextToFile = T.split('|||')
+            # print(TextToFile[1])
+            try:
+                # Insert Text to Rows a.k.a Column A
+                worksheet.write('A'+str(numberA), TextToFile[0])
 
-            # Insert Text to Row Excel
-            worksheet.write('A'+str(numberA), TextToFile[0])
-            worksheet.write('C'+str(numberC), TextToFile[1])
-            
-            numberC += 1
-            numberA += 1
-            numberB += 1
+                # Parameters Image to Row Excel
+                filename = f"{folderImagesRoot}/{TextToFile[0]}.png"
+                img = Image.open(filename)
+                image_width, image_height = img.size
+                y_scale = cell_height/image_height
+                cellImage = 'B' + str(numberB)
+                # Insert Image to Row Excel
+                worksheet.insert_image(cellImage, filename, {'x_scale': y_scale, 'y_scale': y_scale})
 
-        except FileNotFoundError:
-            continue
+                # Insert Text to Rows a.k.a Column C
+                worksheet.write('C'+str(numberC), TextToFile[1])
+                
+                numberC += 1
+                numberA += 1
+                numberB += 1
 
+            except FileNotFoundError:
+                continue
+    except KeyboardInterrupt:
+        print(KeyboardInterrupt)
+        workbook.close()
     workbook.close()
 
-pathNameExcelRoot = "Mazekaz"+".xlsx"
-folderImages = '32'
+pathNameExcelRoot = "Label_CMT_20_03_2021_Real"+".xlsx"
+folderImagesRoot = '32'
 pathNameExcelWrite = "ImageToExcel"+".xlsx"
 
 """
 pathNameExcelRoot = 'images - Copy.xlsx'
-folderImages = 'imagesDATA'
+folderImagesRoot = 'imagesDATA'
 pathNameExcelWrite = 'ImagesToExcel.xlsx'
 """
 
 # ===>> Uncomment and Run here <<====
-# ResizeImages(pathNameExcelRoot, folderImages, pathNameExcelWrite)
+ExportImagesToExcel(pathNameExcelRoot, folderImagesRoot, pathNameExcelWrite)
 
 
 

adf2d8daea8a91d85ea361d6ade19456ae810d8b
Mazekaz <zanym1102@gmail.com>
2021-03-26 17:06:29 +0700

    Upgrade bg_color CellImageNotFound

--- ImageToExcel_ExportLines.py
+++ ImageToExcel_ExportLines.py
@@ -7,19 +7,21 @@ import xlsxwriter
 from PIL import Image
 
 # ===> I'm here <===
+fileNameExtensionExcel = ".xlsx"
 # Start information 
-pathNameExcelRoot = "Label_CMT_20_03_2021_Real.xlsx" # Excel File Root
-folderImagesRoot = "31" # Folder Images Root
-chunkSize = 1000 # Number Line in Files Export  
+pathNameExcelRoot = "Label_CMT_20_03_2021_Real"+fileNameExtensionExcel # Excel File Root
+folderImagesRoot = "33" # Folder Images Root
+chunkSize = 200 # Number Line in Files Export  
 # End information
 
 # Get Folder contains Images Root
 def TotalImages(folderImagesRoot):
-    listNameImages = [path.basename(x).split('.')[0] for x in glob(folderImagesRoot +'/'+ '*.png')]
+    listNameImages = [int(path.basename(x).split('.')[0]) for x in glob(folderImagesRoot +'/'+ '*.png')]
     listNameImages.sort()
-    minListNameImage = int(listNameImages[0])
-    maxListNameImage = int(listNameImages[-1])
+    minListNameImage = listNameImages[0]
+    maxListNameImage = listNameImages[-1]
 
+    del listNameImages
     return minListNameImage, maxListNameImage
 
 def ReadExcelRoot(pathNameExcelRoot, folderImagesRoot):
@@ -52,7 +54,7 @@ def ReadExcelRoot(pathNameExcelRoot, folderImagesRoot):
                 DataExport = {}
                 while startValueCellRow <= maxListNameImage:
                     idValueCellRow = int(WBsheetData.cell(row=cell_obj, column=1).value)
-                    textValueCellRow = (WBsheetData.cell(row=cell_obj, column=2).value)
+                    textValueCellRow = str(WBsheetData.cell(row=cell_obj, column=2).value)
                     
                     # Set Value to Dictionary
                     DataExport[idValueCellRow]=textValueCellRow
@@ -76,46 +78,55 @@ def ChunkData(pathNameExcelRoot, folderImagesRoot, chunkSize):
         yield {k:Data[k] for k in islice(it, chunkSize)}
 
 def ExportToExcels(pathNameExcelRoot, folderImagesRoot, chunkSize):
-    # Create folder contain Excels safely
-    ExportExcelFolder = "ExportExcelFolder"
-    Path(ExportExcelFolder).mkdir(parents=True, exist_ok=True)
-    
-    for item in ChunkData(pathNameExcelRoot, folderImagesRoot, chunkSize):
-        # Name Excel File Output 
-        pathNameExcelWrite = next(iter(item))
+    try: 
+        # Create folder contain Excels safely
+        ExportExcelFolder = "ExportExcelFolder"
+        Path(ExportExcelFolder).mkdir(parents=True, exist_ok=True)
 
-        # Create an new Excel file and add a worksheet
-        workbook = xlsxwriter.Workbook(f"{ExportExcelFolder}/{pathNameExcelWrite}.xlsx")
-        worksheet = workbook.add_worksheet()
+        for item in ChunkData(pathNameExcelRoot, folderImagesRoot, chunkSize):
+            # Name Excel File Output 
+            pathNameExcelWrite = next(iter(item))
 
-        # Height Image and Cell
-        cell_height = 30.0
+            # Create an new Excel file and add a worksheet
+            workbook = xlsxwriter.Workbook(f"{ExportExcelFolder}/{pathNameExcelWrite}{fileNameExtensionExcel}")
+            worksheet = workbook.add_worksheet()
 
-        # Set Default Row
-        worksheet.set_default_row(int(cell_height))
-        worksheet.set_column(1,1, 50)
-        numRow = 1
-        for k, v in item.items():
-            # Insert Text to Rows a.k.a Column A
-            worksheet.write_string('A'+str(numRow), str(k))
+            # Cell Format Image Not Found
+            cell_format = workbook.add_format({'bold': True, 'bg_color': 'red', 'font_color': 'white'})
 
-            # Parameters Image to Row Excel
-            cellImage = 'B' + str(numRow)
-            filename = f"{folderImagesRoot}/{k}.png"
-            img = Image.open(filename)
-            image_width, image_height = img.size
-            y_scale = cell_height/image_height
-            # Insert Image to Row Excel
-            worksheet.insert_image(cellImage, filename, {'x_scale': y_scale, 'y_scale': y_scale})
+            # Height Image and Cell
+            cell_height = 30.0
 
-            # Insert Text to Rows a.k.a Column C
-            worksheet.write_string('C'+str(numRow), str(v))
+            # Set Default Row
+            worksheet.set_default_row(int(cell_height))
+            worksheet.set_column(1,1, 50)
+            numRow = 1
+            for k, v in item.items():
+                # Insert Text to Rows a.k.a Column A
+                worksheet.write_string('A'+str(numRow), str(k))
 
-            numRow += 1
-        # pathNameExcelWrite += 1
-        workbook.close()
-        print('Ting Ting\n Export:', pathNameExcelWrite)
-    print("Mission Success")
+                try:
+                    # Parameters Image to Row Excel
+                    cellImage = 'B' + str(numRow)
+                    filename = f"{folderImagesRoot}/{k}.png"
+                    img = Image.open(filename)
+                    image_width, image_height = img.size
+                    y_scale = cell_height/image_height
+                    # Insert Image to Row Excel
+                    worksheet.insert_image(cellImage, filename, {'x_scale': y_scale, 'y_scale': y_scale})
+                except FileNotFoundError:
+                    worksheet.write_string('B'+str(numRow), 'Image Not Found', cell_format)
+
+                # Insert Text to Rows a.k.a Column C
+                worksheet.write_string('C'+str(numRow), str(v))
+
+                numRow += 1
+            # pathNameExcelWrite += 1
+            workbook.close()
+            print(f'Ting Ting\n + Exported: {pathNameExcelWrite}{fileNameExtensionExcel}')
+        print("Mission Success!!!")
+    except (IndexError, FileNotFoundError, EOFError) as Error:
+        print(f"{Error}\n- Check again: \n + pathNameExcelRoot: {pathNameExcelRoot} \n + folderImagesRoot: {folderImagesRoot}")
 
 # ===> I'm here <===
 ExportToExcels(pathNameExcelRoot, folderImagesRoot, chunkSize)
